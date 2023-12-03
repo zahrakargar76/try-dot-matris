3f3ce1ef1c98ee274abd8eb92b1d9f589554c900
zahrakargar76 <148703264+zahrakargar76@users.noreply.github.com>
2023-12-03 07:46:37 -0800

    Add files via upload

---
+++ try_Dot_Matris.ino
@@ -0,0 +1,29 @@
+#include <MaxMatrix.h>
+int DIN = 7;
+int CLK = 6;
+int CS = 5;
+int maxInUse = 1;
+
+byte buffer [20];
+char text[] = "a";
+
+MaxMatrix m(DIN, CS, CLK, maxInUse);
+void setup() {
+  // put your setup code here, to run once:
+  m.init();
+  m.setIntensity(8);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  m.setDot(0, 7, true);
+  delay(1000);
+  m.setDot(7, 0, true);
+  delay(1000);
+  m.setColumn(3, B11110000);
+  delay(1000);
+  m.setColumn(4, B00001111);
+  delay(1000);
+  m.clear();
+  delay(1000);
+}
